be5cea996710cb8432c082a43d934e4f78398ebc
Carlos Pinto <pilltime@protonmail.com>
2025-02-16 15:08:41 +0000

    create a window

--- src/main.c
+++ src/main.c
@@ -1,36 +1,41 @@
-#include <stdio.h>
 #include <stdlib.h>
 
-#define CGLM_FORCE_DEPTH_ZERO_TO_ONE
-
 #include <GLFW/glfw3.h>
-#include <cglm/cglm.h>
-#include <vulkan/vulkan.h>
 
-int main()
+GLFWwindow *g_window;
+
+void vicInitWindow()
 {
     glfwInit();
-
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *const window = glfwCreateWindow(1280, 720, "Vic", nullptr, nullptr);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    g_window = glfwCreateWindow(1280, 720, "Vic", nullptr, nullptr);
+}
 
-    uint32_t extensions_count = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, nullptr);
-    printf("%d extensions supported\n", extensions_count);
+void vicInitVulkan()
+{
+}
 
-    mat4 matrix;
-    glm_mat4_zero(matrix);
-    vec4 vector;
-    glm_vec4_zero(vector);
-    vec4 result;
-    glm_mat4_mulv(matrix, vector, result);
-
-    while (!glfwWindowShouldClose(window))
+void vicMainLoop()
+{
+    while (!glfwWindowShouldClose(g_window))
     {
         glfwPollEvents();
     }
+}
 
-    glfwDestroyWindow(window);
+void vicCleanup()
+{
+    glfwDestroyWindow(g_window);
     glfwTerminate();
+}
+
+int main()
+{
+    vicInitWindow();
+    vicInitVulkan();
+    vicMainLoop();
+    vicCleanup();
+
     return EXIT_SUCCESS;
 }
