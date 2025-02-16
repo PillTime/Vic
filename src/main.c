cd0ca225574cb9bee2e9c023c937982936196eba
Carlos Pinto <pilltime@protonmail.com>
2025-02-16 15:41:31 +0000

    add a vulkan instance

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 GLFWwindow *g_window;
+VkInstance g_instance;
+
+void vicDie(char const *const msg)
+{
+    fputs(msg, stderr);
+    fflush(stderr);
+    abort();
+}
 
 void vicInitWindow()
 {
@@ -14,6 +26,39 @@ void vicInitWindow()
 
 void vicInitVulkan()
 {
+    VkApplicationInfo const app_info = {.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+                                        .pApplicationName = "Vic",
+                                        .applicationVersion = VK_MAKE_API_VERSION(0, 0, 0, 1),
+                                        .apiVersion = VK_API_VERSION_1_4};
+
+    uint32_t glfw_extensions_count = 0;
+    char const *const *const glfw_extensions =
+        glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
+
+    VkInstanceCreateInfo const create_info = {.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+                                              .pApplicationInfo = &app_info,
+                                              .enabledExtensionCount = glfw_extensions_count,
+                                              .ppEnabledExtensionNames = glfw_extensions,
+                                              .enabledLayerCount = 0};
+
+    uint32_t extensions_count = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, nullptr);
+    VkExtensionProperties *const extensions =
+        malloc(extensions_count * sizeof(VkExtensionProperties));
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, extensions);
+
+    printf("%" PRIu32 " extensions available:", extensions_count);
+    for (uint32_t i = 0; i < extensions_count; i++)
+    {
+        printf("\n\t%s", extensions[i].extensionName);
+    }
+    printf("\n");
+    free(extensions);
+
+    if (vkCreateInstance(&create_info, nullptr, &g_instance) != VK_SUCCESS)
+    {
+        vicDie("failed to create vulkan instance");
+    }
 }
 
 void vicMainLoop()
@@ -26,6 +71,8 @@ void vicMainLoop()
 
 void vicCleanup()
 {
+    vkDestroyInstance(g_instance, nullptr);
+
     glfwDestroyWindow(g_window);
     glfwTerminate();
 }
