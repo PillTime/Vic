0b436aea776b6de63fd5ada8f4fe348fc5da7fdc
Carlos Pinto <pilltime@protonmail.com>
2025-02-17 19:59:13 +0000

    create a window surface

--- src/main.c
+++ src/main.c
@@ -5,12 +5,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define GLFW_INCLUDE_VULKAN
+
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
 
 uint32_t const k_WINDOW_WIDTH = 1280;
 uint32_t const k_WINDOW_HEIGHT = 720;
 char const *const k_APP_NAME = "vic";
+char const *const k_DEVICE_EXTENSIONS[] = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+size_t const k_DEVICE_EXTENSIONS_COUNT = sizeof(k_DEVICE_EXTENSIONS) / sizeof(char const *);
 
 #ifndef NDEBUG
 char const *const k_VALIDATION_LAYERS[] = {"VK_LAYER_KHRONOS_validation"};
@@ -21,7 +25,15 @@ GLFWwindow *g_window;
 VkInstance g_instance;
 VkPhysicalDevice g_physical_device = VK_NULL_HANDLE;
 VkDevice g_device;
+VkSurfaceKHR g_surface;
 VkQueue g_graphics_queue;
+VkQueue g_present_queue;
+VkSwapchainKHR g_swap_chain;
+VkImage *g_swap_chain_images;
+VkImageView *g_swap_chain_image_views;
+uint32_t g_swap_chain_images_count;
+VkFormat g_swap_chain_image_format;
+VkExtent2D g_swap_chain_extent;
 
 #ifndef NDEBUG
 VkDebugUtilsMessengerEXT g_debug_messenger;
@@ -31,24 +43,43 @@ typedef struct S_QueueFamilyIndices
 {
     bool has_graphics_family;
     uint32_t graphics_family;
+    bool has_present_family;
+    uint32_t present_family;
 } QueueFamilyIndices;
 
 QueueFamilyIndices vicNewQueueFamilyIndices()
 {
-    return (QueueFamilyIndices){.has_graphics_family = false};
+    return (QueueFamilyIndices){.has_graphics_family = false, .has_present_family = false};
 }
 
 bool vicQueueFamilyIndicesIsComplete(QueueFamilyIndices const *const indices)
 {
-    return indices->has_graphics_family;
+    return indices->has_graphics_family && indices->has_present_family;
 }
+
+typedef struct S_SwapChainSupportDetails
+{
+    VkSurfaceCapabilitiesKHR capabilities;
+    VkSurfaceFormatKHR *formats;
+    uint32_t formats_count;
+    VkPresentModeKHR *present_modes;
+    uint32_t present_modes_count;
+} SwapChainSupportDetails;
 
 void vicCreateInstance();
 char **vicGetRequiredExtensions_DM(uint32_t *const);
+void vicCreateSurface();
 void vicPickPhysicalDevice();
 bool vicDeviceIsSuitable(VkPhysicalDevice const);
 QueueFamilyIndices vicFindQueueFamilies(VkPhysicalDevice const);
 void vicCreateLogicalDevice();
+bool vicDeviceExtensionsSupported(VkPhysicalDevice const);
+SwapChainSupportDetails vicQuerySwapChainSupport_DM(VkPhysicalDevice const);
+VkSurfaceFormatKHR vicChooseSwapSurfaceFormat(VkSurfaceFormatKHR const *const, size_t const);
+VkPresentModeKHR vicChooseSwapPresentMode(VkPresentModeKHR const *const, size_t const);
+VkExtent2D vicChooseSwapExtent(VkSurfaceCapabilitiesKHR const *const);
+void vicCreateSwapChain_DM();
+void vicCreateImageViews_DM();
 
 #ifndef NDEBUG
 void vicPopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT *const);
@@ -66,6 +97,11 @@ VKAPI_ATTR VkBool32 VKAPI_CALL vicDebugCallback(VkDebugUtilsMessageSeverityFlagB
                                                 VkDebugUtilsMessengerCallbackDataEXT const *const,
                                                 void *const);
 #endif
+
+uint32_t vicClamp(uint32_t const min, uint32_t const x, uint32_t const max)
+{
+    return x < min ? min : x > max ? max : x;
+}
 
 void vicDie(char const *const msg)
 {
@@ -88,8 +124,11 @@ void vicInitVulkan()
 #ifndef NDEBUG
     vicSetupDebugMessenger();
 #endif
+    vicCreateSurface();
     vicPickPhysicalDevice();
     vicCreateLogicalDevice();
+    vicCreateSwapChain_DM();
+    vicCreateImageViews_DM();
 }
 
 void vicMainLoop()
@@ -102,12 +141,22 @@ void vicMainLoop()
 
 void vicCleanup()
 {
+    for (size_t i = 0; i < g_swap_chain_images_count; i++)
+    {
+        vkDestroyImageView(g_device, g_swap_chain_image_views[i], nullptr);
+    }
+    free(g_swap_chain_image_views);
+
+    free(g_swap_chain_images);
+    vkDestroySwapchainKHR(g_device, g_swap_chain, nullptr);
+
     vkDestroyDevice(g_device, nullptr);
 
 #ifndef NDEBUG
     vicDestroyDebugUtilsMessengerEXT(g_instance, g_debug_messenger, nullptr);
 #endif
 
+    vkDestroySurfaceKHR(g_instance, g_surface, nullptr);
     vkDestroyInstance(g_instance, nullptr);
 
     glfwDestroyWindow(g_window);
@@ -205,6 +254,14 @@ char **vicGetRequiredExtensions_DM(uint32_t *const size)
     return extensions;
 }
 
+void vicCreateSurface()
+{
+    if (glfwCreateWindowSurface(g_instance, g_window, nullptr, &g_surface) != VK_SUCCESS)
+    {
+        vicDie("failed to create window surface");
+    }
+}
+
 void vicPickPhysicalDevice()
 {
     uint32_t devices_count = 0;
@@ -242,8 +299,22 @@ bool vicDeviceIsSuitable(VkPhysicalDevice const device)
 
     QueueFamilyIndices const indices = vicFindQueueFamilies(device);
 
+    bool const extensions_supported = vicDeviceExtensionsSupported(device);
+
+    bool swap_chain_adequate = false;
+    if (extensions_supported)
+    {
+        SwapChainSupportDetails const swap_chain_support = vicQuerySwapChainSupport_DM(device);
+        swap_chain_adequate =
+            swap_chain_support.formats_count != 0 && swap_chain_support.present_modes_count != 0;
+
+        free(swap_chain_support.formats);
+        free(swap_chain_support.present_modes);
+    }
+
     return properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
-           features.geometryShader && vicQueueFamilyIndicesIsComplete(&indices);
+           features.geometryShader && vicQueueFamilyIndicesIsComplete(&indices) &&
+           extensions_supported && swap_chain_adequate;
 }
 
 QueueFamilyIndices vicFindQueueFamilies(VkPhysicalDevice const device)
@@ -256,12 +327,23 @@ QueueFamilyIndices vicFindQueueFamilies(VkPhysicalDevice const device)
         calloc((size_t)queue_families_count, sizeof(VkQueueFamilyProperties));
     vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, queue_families);
 
+    // TODO: check why validation layers complain about non unique queue family indices
     for (size_t i = 0; i < queue_families_count; i++)
     {
         if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
         {
             indices.graphics_family = i;
             indices.has_graphics_family = true;
+            continue;
+        }
+
+        VkBool32 present_support = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, g_surface, &present_support);
+        if (present_support)
+        {
+            indices.present_family = i;
+            indices.has_present_family = true;
+            continue;
         }
 
         if (vicQueueFamilyIndicesIsComplete(&indices))
@@ -278,22 +360,31 @@ void vicCreateLogicalDevice()
 {
     QueueFamilyIndices const indices = vicFindQueueFamilies(g_physical_device);
 
-    VkDeviceQueueCreateInfo queue_create_info = {};
-    queue_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-    queue_create_info.queueFamilyIndex = indices.graphics_family;
-    queue_create_info.queueCount = 1;
+    uint32_t const unique_queue_families[] = {indices.graphics_family, indices.present_family};
+    size_t const unique_queue_families_count = sizeof(unique_queue_families) / sizeof(uint32_t);
+
+    VkDeviceQueueCreateInfo *const queue_create_infos =
+        calloc(unique_queue_families_count, sizeof(VkDeviceQueueCreateInfo));
 
     float const queue_priority = 1.0;
-    queue_create_info.pQueuePriorities = &queue_priority;
+    for (size_t i = 0; i < unique_queue_families_count; i++)
+    {
+        queue_create_infos[i] = (VkDeviceQueueCreateInfo){};
+        queue_create_infos[i].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queue_create_infos[i].queueFamilyIndex = unique_queue_families[i];
+        queue_create_infos[i].queueCount = 1;
+        queue_create_infos[i].pQueuePriorities = &queue_priority;
+    }
 
     VkPhysicalDeviceFeatures const device_features = {};
 
     VkDeviceCreateInfo create_info = {};
     create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-    create_info.pQueueCreateInfos = &queue_create_info;
-    create_info.queueCreateInfoCount = 1;
+    create_info.pQueueCreateInfos = queue_create_infos;
+    create_info.queueCreateInfoCount = (uint32_t)unique_queue_families_count;
     create_info.pEnabledFeatures = &device_features;
-    create_info.enabledExtensionCount = 0;
+    create_info.enabledExtensionCount = (uint32_t)k_DEVICE_EXTENSIONS_COUNT;
+    create_info.ppEnabledExtensionNames = k_DEVICE_EXTENSIONS;
 #ifndef NDEBUG
     create_info.enabledLayerCount = (uint32_t)k_VALIDATION_LAYERS_COUNT;
     create_info.ppEnabledLayerNames = k_VALIDATION_LAYERS;
@@ -307,6 +398,211 @@ void vicCreateLogicalDevice()
     }
 
     vkGetDeviceQueue(g_device, indices.graphics_family, 0, &g_graphics_queue);
+    vkGetDeviceQueue(g_device, indices.present_family, 0, &g_present_queue);
+
+    free(queue_create_infos);
+}
+
+bool vicDeviceExtensionsSupported(VkPhysicalDevice const device)
+{
+    uint32_t extensions_count = 0;
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensions_count, nullptr);
+    VkExtensionProperties *const extensions =
+        calloc(extensions_count, sizeof(VkExtensionProperties));
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensions_count, extensions);
+
+    bool all_extensions_available = true;
+
+    for (size_t i = 0; i < k_DEVICE_EXTENSIONS_COUNT; i++)
+    {
+        bool extension_found = false;
+        for (size_t j = 0; j < extensions_count; j++)
+        {
+            if (strcmp(k_DEVICE_EXTENSIONS[i], extensions[j].extensionName) == 0)
+            {
+                extension_found = true;
+                break;
+            }
+        }
+        all_extensions_available &= extension_found;
+    }
+
+    free(extensions);
+    return all_extensions_available;
+}
+
+SwapChainSupportDetails vicQuerySwapChainSupport_DM(VkPhysicalDevice const device)
+{
+    SwapChainSupportDetails details = {};
+
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, g_surface, &details.capabilities);
+
+    vkGetPhysicalDeviceSurfaceFormatsKHR(device, g_surface, &details.formats_count, nullptr);
+    if (details.formats_count != 0)
+    {
+        details.formats = calloc(details.formats_count, sizeof(VkSurfaceFormatKHR));
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, g_surface, &details.formats_count,
+                                             details.formats);
+    }
+
+    vkGetPhysicalDeviceSurfacePresentModesKHR(device, g_surface, &details.present_modes_count,
+                                              nullptr);
+    if (details.present_modes_count != 0)
+    {
+        details.present_modes = calloc(details.present_modes_count, sizeof(VkPresentModeKHR));
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, g_surface, &details.present_modes_count,
+                                                  details.present_modes);
+    }
+
+    return details;
+}
+
+VkSurfaceFormatKHR vicChooseSwapSurfaceFormat(VkSurfaceFormatKHR const *const available_formats,
+                                              size_t const available_formats_count)
+{
+    for (size_t i = 0; i < available_formats_count; i++)
+    {
+        if (available_formats[i].format == VK_FORMAT_B8G8R8A8_SRGB &&
+            available_formats[i].colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+        {
+            return available_formats[i];
+        }
+    }
+
+    return available_formats[0];
+}
+
+VkPresentModeKHR vicChooseSwapPresentMode(VkPresentModeKHR const *const available_present_modes,
+                                          size_t const available_present_modes_count)
+{
+    for (size_t i = 0; i < available_present_modes_count; i++)
+    {
+        if (available_present_modes[i] == VK_PRESENT_MODE_MAILBOX_KHR)
+        {
+            return available_present_modes[i];
+        }
+    }
+
+    return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+VkExtent2D vicChooseSwapExtent(VkSurfaceCapabilitiesKHR const *const capabilities)
+{
+    if (capabilities->currentExtent.width != UINT32_MAX)
+    {
+        return capabilities->currentExtent;
+    }
+    else
+    {
+        int width = 0, height = 0;
+        glfwGetFramebufferSize(g_window, &width, &height);
+
+        VkExtent2D actual_extent = {
+            .width = (uint32_t)width,
+            .height = (uint32_t)height,
+        };
+
+        actual_extent.width = vicClamp(capabilities->minImageExtent.width, actual_extent.width,
+                                       capabilities->maxImageExtent.width);
+        actual_extent.height = vicClamp(capabilities->minImageExtent.height, actual_extent.height,
+                                        capabilities->maxImageExtent.height);
+
+        return actual_extent;
+    }
+}
+
+void vicCreateSwapChain_DM()
+{
+    SwapChainSupportDetails const swap_chain_support =
+        vicQuerySwapChainSupport_DM(g_physical_device);
+
+    VkSurfaceFormatKHR const surface_format =
+        vicChooseSwapSurfaceFormat(swap_chain_support.formats, swap_chain_support.formats_count);
+    VkPresentModeKHR const present_mode = vicChooseSwapPresentMode(
+        swap_chain_support.present_modes, swap_chain_support.present_modes_count);
+    VkExtent2D const extent = vicChooseSwapExtent(&swap_chain_support.capabilities);
+
+    uint32_t image_count = swap_chain_support.capabilities.minImageCount + 1;
+    if (swap_chain_support.capabilities.maxImageCount > 0 &&
+        image_count > swap_chain_support.capabilities.maxImageCount)
+    {
+        image_count = swap_chain_support.capabilities.maxImageCount;
+    }
+
+    VkSwapchainCreateInfoKHR create_info = {};
+    create_info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    create_info.surface = g_surface;
+    create_info.minImageCount = image_count;
+    create_info.imageFormat = surface_format.format;
+    create_info.imageColorSpace = surface_format.colorSpace;
+    create_info.imageExtent = extent;
+    create_info.imageArrayLayers = 1;
+    create_info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+    QueueFamilyIndices const indices = vicFindQueueFamilies(g_physical_device);
+    uint32_t const queue_family_indices[] = {indices.graphics_family, indices.present_family};
+
+    if (indices.graphics_family != indices.present_family)
+    {
+        create_info.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        create_info.queueFamilyIndexCount = 2;
+        create_info.pQueueFamilyIndices = queue_family_indices;
+    }
+    else
+    {
+        // TODO: same as in vicFindQueueFamilyIndices (this block will never run)
+        create_info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    }
+
+    create_info.preTransform = swap_chain_support.capabilities.currentTransform;
+    create_info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+    create_info.presentMode = present_mode;
+    create_info.clipped = VK_TRUE;
+    create_info.oldSwapchain = VK_NULL_HANDLE;
+
+    if (vkCreateSwapchainKHR(g_device, &create_info, nullptr, &g_swap_chain) != VK_SUCCESS)
+    {
+        vicDie("failed to create swap chain");
+    }
+
+    vkGetSwapchainImagesKHR(g_device, g_swap_chain, &g_swap_chain_images_count, nullptr);
+    g_swap_chain_images = calloc(g_swap_chain_images_count, sizeof(VkImage));
+    vkGetSwapchainImagesKHR(g_device, g_swap_chain, &g_swap_chain_images_count,
+                            g_swap_chain_images);
+
+    g_swap_chain_image_format = surface_format.format;
+    g_swap_chain_extent = extent;
+
+    free(swap_chain_support.formats);
+    free(swap_chain_support.present_modes);
+}
+
+void vicCreateImageViews_DM()
+{
+    g_swap_chain_image_views = calloc(g_swap_chain_images_count, sizeof(VkImageView));
+    for (size_t i = 0; i < g_swap_chain_images_count; i++)
+    {
+        VkImageViewCreateInfo create_info = {};
+        create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        create_info.image = g_swap_chain_images[i];
+        create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        create_info.format = g_swap_chain_image_format;
+        create_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+        create_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+        create_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+        create_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+        create_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        create_info.subresourceRange.baseMipLevel = 0;
+        create_info.subresourceRange.levelCount = 1;
+        create_info.subresourceRange.baseArrayLayer = 0;
+        create_info.subresourceRange.layerCount = 1;
+
+        if (vkCreateImageView(g_device, &create_info, nullptr, &g_swap_chain_image_views[i]) !=
+            VK_SUCCESS)
+        {
+            vicDie("failed to create image views");
+        }
+    }
 }
 
 #ifndef NDEBUG
